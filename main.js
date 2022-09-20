67d33bd04d4457f97581e0e76abfb81a14e13f02
David Lara <david.a.lara05@gmail.com>
2022-09-20 11:04:51 -0500

    final changes

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 "use strict"
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+let coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
+
 // We change tr th to div,P, and h2
 function renderCoffee(coffee) {
     let html = '<div class="coffee">';
@@ -31,26 +50,6 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
-
-
-
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-let coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
 
 
 
@@ -97,5 +96,6 @@ let coffeeName = document.querySelector('#userInput');
 let roastAdd = document.querySelector('#roast-selection2');
 let submitButton2 = document.querySelector('#submit2');
 submitButton2.addEventListener('click', addCoffee);
-
+submitButton2.addEventListener('click', updateCoffees);
+roastSelection.addEventListener("change", updateCoffees)
 
